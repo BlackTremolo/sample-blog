cc420cdcd02ec35cb3182434c4dfcdd16dd0bdda
BlackTremolo <mihsee436@gmail.com>
2025-12-13 14:30:04 +0800

    Создание actions for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,33 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+  def show
+  @article_show = Article.find(1) 
+  end
+
+  def edit
+
   end
 end
+
+private
+
+def article_params
+  params.require(:article).permit(:title, :text)
+end
+
