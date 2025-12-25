3eeec0d31b8dab109a4e5e286900989be5cc2031
BlackTremolo <mihsee436@gmail.com>
2025-12-25 21:44:33 +0800

    Улучшение кода для Article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 describe Article, type: :model do
+  describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:text) }
+  end
+    
+  describe 'association' do
     it { should have_many(:comments)}
+  end
 end
